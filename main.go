0c5d677ef9ded04bfc5857ce3506e0d00dce36dc
Tong Zhang <zhangt@frib.msu.edu>
2025-11-25 10:51:07 -0500

    REF: Update app config.

--- main.go
+++ main.go
@@ -42,19 +42,18 @@ func main() {
 	// Create application with options
 	err = wails.Run(&options.App{
 		Title:  "IndicoDataFusion",
-		Width:  1024,
-		Height: 768,
+		Width:  1280,
+		Height: 800,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		OnStartup: app.startup,
 		Bind: []interface{}{
 			app,
 			indicoClient,
 		},
+		EnableDefaultContextMenu: false,
 	})
-
 	if err != nil {
 		println("Error:", err.Error())
 	}
