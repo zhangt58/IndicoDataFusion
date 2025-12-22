9f0f650e56842e81ff332d05ff3f69f342e198a8
Tong Zhang <zhangt@frib.msu.edu>
2025-12-22 10:34:52 -0500

    REF: Adjust the window round corners.

--- frontend/src/components/TitleBar.svelte
+++ frontend/src/components/TitleBar.svelte
@@ -52,7 +52,7 @@
 </script>
 
 <div
-  class="titlebar select-none fixed top-0 left-0 right-0 flex items-center justify-between h-10 bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700 shadow-md dark:shadow-black/40"
+  class="titlebar select-none fixed top-0 left-0 right-0 rounded-t-xl flex items-center justify-between h-10 bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700 shadow-md dark:shadow-black/40"
   style="--wails-draggable: drag; z-index: 9999;"
   role="button"
   tabindex="0"
