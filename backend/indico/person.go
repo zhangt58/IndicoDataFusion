1eceb7852f357bc06c1f7483ecb9d2c470be9aba
Tong Zhang <warriorlance@gmail.com>
2026-01-03 12:29:15 -0500

    ENH: Add methods for affiliation data unmarshalling.

--- backend/indico/person.go
+++ backend/indico/person.go
@@ -1,5 +1,7 @@
 package indico
 
+import "encoding/json"
+
 // Affiliation represents a structured affiliation object for people and judges
 type Affiliation struct {
 	City        string `json:"city"`
@@ -13,7 +15,7 @@ type Affiliation struct {
 
 // Person represents a person in an abstract (author, speaker, etc.)
 type Person struct {
-	Affiliation *Affiliation `json:"affiliation"`
+	Affiliation *Affiliation `json:"-"` // populated from affiliation_link
 	Email       string       `json:"email"`
 	AuthorType  string       `json:"author_type"`
 	FirstName   string       `json:"first_name"`
@@ -22,9 +24,25 @@ type Person struct {
 	PersonID    int          `json:"person_id"`
 }
 
+// UnmarshalJSON custom unmarshaler for Person to extract affiliation from affiliation_link
+func (p *Person) UnmarshalJSON(data []byte) error {
+	type Alias Person
+	aux := &struct {
+		AffiliationLink *Affiliation `json:"affiliation_link"`
+		*Alias
+	}{
+		Alias: (*Alias)(p),
+	}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+	p.Affiliation = aux.AffiliationLink
+	return nil
+}
+
 // Judge represents the judge information for an abstract
 type Judge struct {
-	Affiliation *Affiliation `json:"affiliation"`
+	Affiliation *Affiliation `json:"-"` // populated from affiliation_meta
 	Email       string       `json:"email"`
 	FirstName   string       `json:"first_name"`
 	LastName    string       `json:"last_name"`
@@ -34,10 +52,26 @@ type Judge struct {
 	Identifier  string       `json:"identifier"`
 }
 
+// UnmarshalJSON custom unmarshaler for Judge to extract affiliation from affiliation_meta
+func (j *Judge) UnmarshalJSON(data []byte) error {
+	type Alias Judge
+	aux := &struct {
+		AffiliationMeta *Affiliation `json:"affiliation_meta"`
+		*Alias
+	}{
+		Alias: (*Alias)(j),
+	}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+	j.Affiliation = aux.AffiliationMeta
+	return nil
+}
+
 // Submitter represents the person who submitted the abstract
 // Affiliation is a structured Affiliation pointer.
 type Submitter struct {
-	Affiliation *Affiliation `json:"affiliation"`
+	Affiliation *Affiliation `json:"-"` // populated from affiliation_meta
 	Email       string       `json:"email"`
 	FirstName   string       `json:"first_name"`
 	LastName    string       `json:"last_name"`
@@ -45,4 +79,20 @@ type Submitter struct {
 	AvatarURL   string       `json:"avatar_url"`
 	ID          int          `json:"id"`
 	Identifier  string       `json:"identifier"`
+}
+
+// UnmarshalJSON custom unmarshaler for Submitter to extract affiliation from affiliation_meta
+func (s *Submitter) UnmarshalJSON(data []byte) error {
+	type Alias Submitter
+	aux := &struct {
+		AffiliationMeta *Affiliation `json:"affiliation_meta"`
+		*Alias
+	}{
+		Alias: (*Alias)(s),
+	}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+	s.Affiliation = aux.AffiliationMeta
+	return nil
 }
