2c0ddbc7032df319cf54a3f6529f92d8304ebabc
Tong Zhang <zhangt@frib.msu.edu>
2026-02-10 14:20:50 -0500

    MNT: Bump version to 1.3.2.

--- backend/consts/consts.go
+++ backend/consts/consts.go
@@ -3,7 +3,7 @@ package consts
 const (
 	AppName     = "IndicoDataFusion"
 	AppNameAbbr = "IDF"
-	AppVersion  = "v1.3.1"
+	AppVersion  = "v1.3.2"
 	Author      = "Tong Zhang"
 	Department  = "Facility for Rare Isotope Beams"
 	Company     = "Michigan State University"
