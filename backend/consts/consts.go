738afa03ae8df8feca7e01d3aa8dbc6c44187438
Tong Zhang <warriorlance@gmail.com>
2026-03-08 20:23:07 -0400

    MNT: Bump version to 1.6.1.

--- backend/consts/consts.go
+++ backend/consts/consts.go
@@ -3,7 +3,7 @@ package consts
 const (
 	AppName      = "IndicoDataFusion"
 	AppNameAbbr  = "IDF"
-	AppVersion   = "v1.6.0"
+	AppVersion   = "v1.6.1"
 	Author       = "Tong Zhang"
 	Organization = "Facility for Rare Isotope Beams"
 	ConfEnvName  = "IDF_CONFIG_PATH"
